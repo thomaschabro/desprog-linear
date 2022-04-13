b15142bac994dca2cb466bdd407a55d3788d5c11
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2022-04-13 08:25:44 -0300

    Adapt for array implementation

--- queue_int.c
+++ queue_int.c
@@ -3,20 +3,35 @@
 #include "queue_int.h"
 
 struct _queue_int {
+    int capacity;
+    int *data;
+    int begin;
+    int end;
+    int size;
 };
 
-queue_int *queue_int_new() {
+queue_int *queue_int_new(int capacity) {
     queue_int *q = malloc(sizeof(queue_int));
+    q->capacity = capacity;
+    q->data = malloc(capacity * sizeof(int));
+    q->begin = capacity - 1;
+    q->end = 0;
+    q->size = 0;
     return q;
 }
 
 void queue_int_delete(queue_int **_q) {
     queue_int *q = *_q;
+    free(q->data);
     free(q);
     *_q = NULL;
 }
 
 int queue_int_empty(queue_int *q) {
+    return 0;
+}
+
+int queue_int_full(queue_int *q) {
     return 0;
 }
 
--- stack_int.c
+++ stack_int.c
@@ -3,15 +3,22 @@
 #include "stack_int.h"
 
 struct _stack_int {
+    int capacity;
+    int *data;
+    int size;
 };
 
-stack_int *stack_int_new() {
+stack_int *stack_int_new(int capacity) {
     stack_int *s = malloc(sizeof(stack_int));
+    s->capacity = capacity;
+    s->data = malloc(capacity * sizeof(int));
+    s->size = 0;
     return s;
 }
 
 void stack_int_delete(stack_int **_s) {
     stack_int *s = *_s;
+    free(s->data);
     free(s);
     *_s = NULL;
 }
@@ -20,8 +27,13 @@ int stack_int_empty(stack_int *s) {
     return 0;
 }
 
+int stack_int_full(stack_int *s) {
+    return 0;
+}
+
 void stack_int_push(stack_int *s, int value) {
 }
 
 int stack_int_pop(stack_int *s) {
+    return 0;
 }
