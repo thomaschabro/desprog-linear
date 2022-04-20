9980b706f9e474622131eed9812ba56a9f685d8b
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2022-04-20 08:11:37 -0300

    Adapt for list implementation

--- queue_int.c
+++ queue_int.c
@@ -2,22 +2,39 @@
 
 #include "queue_int.h"
 
+struct _node {
+    int value;
+    struct _node *next;
+};
+
+typedef struct _node node;
+
 struct _queue_int {
+    node *first;
+    node *last;
 };
 
 queue_int *queue_int_new() {
     queue_int *q = malloc(sizeof(queue_int));
+    q->first = NULL;
+    q->last = NULL;
     return q;
 }
 
 void queue_int_delete(queue_int **_q) {
     queue_int *q = *_q;
+    node *n = q->first;
+    while (n != NULL) {
+        node *next = n->next;
+        free(n);
+        n = next;
+    }
     free(q);
     *_q = NULL;
 }
 
 int queue_int_empty(queue_int *q) {
-    return 0;
+    return q->last == NULL;
 }
 
 void queue_int_put(queue_int *q, int value) {
